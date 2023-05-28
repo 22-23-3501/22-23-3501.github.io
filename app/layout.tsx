d6666718e8bdd670422379c066cebe330a2d07e2
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2023-05-28 06:22:55 +0000

    add og data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
-import Footer from './componses/footer'
-import Header from './componses/header'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Metadata } from "next"
+import Footer from "./componses/footer"
+import Header from "./componses/header"
+import "./globals.css"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
-  title: '國際3510地區扶輪少年服務團',
-  description: '',
+export const metadata: Metadata = {
+  title: "國際3510地區扶輪少年服務團",
+  description: "",
+  openGraph: {
+    title: "國際3510地區扶輪少年服務團",
+    images: "/logo.png",
+    description: ""
+  } 
 }
 
 export default function RootLayout({
